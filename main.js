0b49357a441c7da139a9d99ccd33543420c3392d
Swartex <andrey92.ua@gmail.com>
2016-06-29 23:30:04 +0300

    Edit function

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
     }
     init();
 
-        function create_snake()
+    function create_snake()
     {
         var length = 4; //Длинна змеи
         snake_array = []; //Пустой массив для старта
